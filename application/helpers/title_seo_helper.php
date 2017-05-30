38103db0f6111bafaf3e58d37812ab5a6ce4a3e5
DIVA-PC\diva <diva@sedana.co.id>
2017-05-30 08:00:28 +0700

    no message

--- application/helpers/title_seo_helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-function title_seo($text){
-	$char_del = array("–","(",")","\"",".",",",":");
-	$text = str_replace($char_del,"",$text);
-	$replaces = array(" ",",","%","@","&","/","?");
-	$uri_text = str_replace($replaces,'-',$text);
-	return strtolower($uri_text);
-}
-?>
--- application/views/openImageVw.php
+++
@@ -1,5 +0,0 @@
-
-												<figure class="image">
-														
-                                            		<img src="<?php echo base_url(); ?>assets/images/upload/<?php echo $fileName;?>" alt="fauzan" class="img-circle" />
-												</figure>
